5596b87f7a38dea9a3f856825dacabd10e17c5b5
sk192 <korulkar.shweta7@gmail.com>
2019-05-21 18:45:17 -0700

    8 queen using Genetic Algorithm

--- 8_Queen_using_Genetic_Algorithm.py
+++ 8_Queen_using_Genetic_Algorithm.py
@@ -9,8 +9,9 @@ import math
 import datetime
 from collections import OrderedDict
 from operator import itemgetter  
+import matplotlib.pyplot as plt
 
-def fitness_function(population, queen_loc):
+def fitness_function(population, queen_loc): 		# caluculate the number of non attacaking pair of queens as fitness value
 	state_fitness_dict = []
 	for i in range(len(queen_loc)):
 		fitness = 0
@@ -30,18 +31,18 @@ def fitness_function(population, queen_loc):
 		state_fitness_dict.append((population[i],fit))
 	return state_fitness_dict
 
-def selection(fit):
+def selection(fit):									# Used roulette wheel selection method. 
 	S=0
 	for k in fit: 
 		S = S + k[1]
 	prob_selection = []
 	parent_selection = []
 	previous_prob = 0.0
-	for key in fit:
-		previous_prob = previous_prob + (key[1]/S)
+	for key in fit:											
+		previous_prob = previous_prob + (key[1]/S)			# calculate probability of every state and add with previous state probability
 		prob_selection.append((key[0], previous_prob))
 	sorted(prob_selection, key= itemgetter(1))
-	for i in range(len(prob_selection)):
+	for i in range(len(prob_selection)):				# Select the parents whose probability is greater than randomly selected number i.e. r 
 		r = random.uniform(0,1)
 		for j in prob_selection:
 			if r < j[1]:
@@ -56,10 +57,10 @@ def crossover(p_sel):
 	j = 1
 	crossover = []
 	while i < len(p_sel) - 1 and j < len(p_sel):
-		c = random.randint(1,6)
+		c = random.randint(1,6)								# select the crossover point randomly.
 		A = p_sel[i]
 		B = p_sel[j]
-		C = A[:c+1] + B[c+1:]
+		C = A[:c+1] + B[c+1:]								# Generate child.
 		D = B[:c+1] + A[c+1:]
 		crossover.append(C)
 		crossover.append(D)
@@ -67,18 +68,18 @@ def crossover(p_sel):
 		j += 2
 	return crossover
 
-def mutation(cross):
-	prob = 0.05
+def mutation(cross):									# Mutation will happen if n < prob.							
+	prob = 0.05									
 	for i in range(len(cross)):
 		n = random.uniform(0,1)
 		if n < prob:
-			while True:
+			while True:									
 				r1 = str(random.randint(1,8))
 				r2 = str(random.randint(1,8))
 				if r1 in cross[i]:
 					cr = cross[i].find(r1)
 					clist = list(cross[i])
-					clist[cr] = r2
+					clist[cr] = r2							# replace the randomly selected number with other number.
 					cross[i] = ''.join(clist)
 					break
 	return cross
@@ -86,32 +87,64 @@ def mutation(cross):
 def process(population):
 
 	S= 0 
-	for i in range(400):
+	count = 0
+	avg_fitness = []
+	generation_population = []
+	for i in range(100):
+		generation_population.append(i+1)
+		count += 1
 		queen_loc = []
 		z = 0
 		while z < len(population):
 			l = 0
 			q_l = []
-			while l < len(population[0]):
+			while l < len(population[0]):					# this loop will create the 2-D queen loaction to calulate the fitness function.
 				q_l.append((int(population[z][l]),l+1))
 				l += 1
-			queen_loc.append(q_l)
+			queen_loc.append(q_l)				
 			z += 1
 		fit = fitness_function(population, queen_loc)
+		sum1 = 0
+		for i in range(len(fit)):
+			sum1 = sum1 + fit[i][1]
+		avg = sum1 / len(fit)
+		avg_fitness.append(avg)
+		print("----------Avg: %s" %(avg))  
 		for h in fit:
-			if 28 == h[1]:
+			if 28 == h[1]:				
 				print("---------------FOUND THE OUTPUT: The state of non attacking queens is:  ---------------")
+				plt.title("Size_of_population = {%s}, Solved_in_genereation = {%s}" %(len(population),count))     # code to draw the graph
+				plt.plot(generation_population, avg_fitness)
+				plt.ylabel("Average fitness per geneartion ")
+				plt.xlabel("Number of generations ")
+				plt.yticks(range(0,30,2))
+				plt.xticks(range(0,100,10))
+				plt.ylim(0,28)
+				plt.tight_layout()
+				plt.show()
+				print("count: %s" %(count))
 				return h[0]
 		
 		p_sel = selection(fit)
 		cross = crossover(p_sel)
 		new_population = mutation(cross)
 		population = new_population
-	print("Sorry could not find solution...Try again ")
+	print("Sorry could not find solution in 100 iterations...Try again ")
+	plt.title("Size_of_population = {%s}, Solved_in_genereation = {%s}" %(len(population),count))
+	plt.plot(generation_population, avg_fitness)
+	plt.ylabel("Average fitness per geneartion ")
+	plt.xlabel("Number of generations ")
+	plt.yticks(range(0,30,2))
+	plt.xticks(range(0,100,10))
+	plt.ylim(0,28)
+	plt.tight_layout()
+	plt.show()
+
+	
 	#return population
 		
 if __name__ == '__main__':
-	n = int(input("Enter number of states in population: "))
+	n = int(input("Enter number of states in population (population size): "))
 	
 	population = []
 	states = [] 
